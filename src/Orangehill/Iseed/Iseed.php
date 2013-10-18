8ac87d63f00c295f42f3efbb718220959927488c
Ivan Batic <ivan.batic@live.com>
2013-10-18 12:47:55 +0200

    Fixed MySQL error 1390 (prepared statement contains too many placeholders).

--- src/Orangehill/Iseed/Iseed.php
+++ src/Orangehill/Iseed/Iseed.php
@@ -39,7 +39,7 @@ class Iseed {
 		$seedsPath = $this->getPath($className, $seedPath);
 
 		// Get a populated stub file
-		$seedContent = $this->populateStub($className, $stub, $table, $this->prettifyArray($dataArray));
+		$seedContent = $this->populateStub($className, $stub, $table, $dataArray);
 
 		// Save a populated stub
 		$this->files->put($seedsPath, $seedContent);
@@ -128,17 +128,23 @@ class Iseed {
 	 */
 	public function populateStub($class, $stub, $table, $data)
 	{
-		$stub = str_replace('{{class}}', $class, $stub);
+		$inserts = '';
+        $chunks = array_chunk($data, 500);
+        foreach ($chunks as $chunk) {
+            $inserts .= sprintf("\n\t\t\DB::table('%s')->insert(%s);", $table, $this->prettifyArray($chunk));
+        }
+        
+        $stub = str_replace('{{class}}', $class, $stub);
 
-		if (!is_null($table)) {
-			$stub = str_replace('{{table}}', $table, $stub);
-		}
+        if (!is_null($table)) {
+            $stub = str_replace('{{table}}', $table, $stub);
+        }
 
-		if (!is_null($data)) {
-			$stub = str_replace('{{data}}', $data, $stub);
-		}
+        if ($inserts) {
+            $stub = str_replace('{{insert_statements}}', $inserts, $stub);
+        }
 
-		return $stub;
+        return $stub;
 	}
 
 	/**
