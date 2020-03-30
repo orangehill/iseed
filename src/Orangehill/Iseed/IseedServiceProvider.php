a93be8316210edb62f17b09b5edb49f8cc628872
Roberto Andrade <roberto.andrade@gmail.com>
2020-03-30 18:57:21 -0300

    Removing it altogether to match latest codebase

--- src/Orangehill/Iseed/IseedServiceProvider.php
+++ src/Orangehill/Iseed/IseedServiceProvider.php
@@ -35,12 +35,6 @@ class IseedServiceProvider extends ServiceProvider
         $this->app['iseed'] = $this->app->share(function($app) {
             return new Iseed;
         });
-        /*
-        $this->app->booting(function() {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias('Iseed', 'Orangehill\Iseed\Facades\Iseed');
-        });
-        */
 
         $this->app['command.iseed'] = $this->app->share(function($app) {
             return new IseedCommand;
